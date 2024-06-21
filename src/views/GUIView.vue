2e6c49cfdae1dacbc3cb72d94215f7266e3df10d
DoshiX <daria.pehteleva@gmail.com>
2024-06-21 21:56:01 +0300

    Реализован компонент иконки и добалены необходимые иконки согласно макеты

--- src/views/GUIView.vue
+++ src/views/GUIView.vue
@@ -7,8 +7,17 @@
         <main class="gui-page__content">
             <div class="gui-page__component-list">
                 <div class="gui-page__component-item">
-                    <h3 class="gui-page__component-title">Компонент 1</h3>
-                        <div>Компонент</div>
+                    <h3 class="gui-page__component-title">Иконки</h3>
+                    <div class="gui-page__component-icons">
+                        <icon-base icon-name="call"><icon-call /></icon-base>
+                        <icon-base icon-name="emoji"><icon-emoji /></icon-base>
+                        <icon-base icon-name="check"><icon-check /></icon-base>
+                        <icon-base icon-name="menu"><icon-menu /></icon-base>
+                        <icon-base icon-name="more"><icon-more /></icon-base>
+                        <icon-base icon-name="search"><icon-search /></icon-base>
+                        <icon-base icon-name="send"><icon-send /></icon-base>
+                        <icon-base icon-name="verified"><icon-verified /></icon-base>
+                    </div>
                 </div>
 
                 <div class="gui-page__component-item">
@@ -20,6 +29,18 @@
     </div>
 </template>
 
+<script setup>
+import IconBase from '@/components/IconBase.vue';
+import IconCall from '@/components/icons/IconCall.vue';
+import IconEmoji from '@/components/icons/IconEmoji.vue';
+import IconCheck from '@/components/icons/IconCheck.vue';
+import IconMenu from '@/components/icons/IconMenu.vue';
+import IconMore from '@/components/icons/IconMore.vue';
+import IconSearch from '@/components/icons/IconSearch.vue';
+import IconSend from '@/components/icons/IconSend.vue';
+import IconVerified from '@/components/icons/IconVerified.vue';
+</script>
+
 <style scoped lang="scss">
 .gui-page {
     line-height: 30px;
@@ -27,7 +48,7 @@
     display: flex;
     flex-direction: column;
 
-    min-height: 140vh;
+    min-height: 100vh;
     max-width: 1280px;
     margin: 0 auto;
 
@@ -71,7 +92,6 @@
             display: flex;
             flex-direction: column;
             gap: 20px;
-
         }
 
         &-item {
@@ -79,11 +99,19 @@
             flex-direction: column;
             gap: 10px;
 
+            padding-bottom: 10px;
+
             border-bottom: 1px dashed #eaecef;
         }
 
         &-title {
             color: #8f8fff;
+        }
+
+        &-icons {
+            display: flex;
+            flex-wrap: wrap;
+            gap: 10px;
         }
     }
 }
